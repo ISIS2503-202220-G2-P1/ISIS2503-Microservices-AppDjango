82295eda6131b885446710c0ec780821007a8e4d
Juan Alegría <juanszalegria@gmail.com>
2022-11-27 14:32:19 -0500

    Fix: update field

--- places/places/models.py
+++ places/places/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Place(models.Model):
-    measurements = models.ManyToOneRel(null=False, default=None)
+    measurements = models.IntegerField(null=False, default=None)
     name = models.CharField(max_length=50)
 
     def __str__(self):
