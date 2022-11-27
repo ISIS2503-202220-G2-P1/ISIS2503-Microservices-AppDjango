cccebea8d4e32d5fd821f8d9d2cd20f33f2d87d2
Juan Alegría <juanszalegria@gmail.com>
2022-11-27 14:33:25 -0500

    Fix: update url call

--- places/places/urls.py
+++ places/places/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    url(r'^places/', views.MeasurementList),
+    url(r'^places/', views.PlacetList),
     url(r'^placecreate/$', csrf_exempt(views.PlaceCreate), name='placeCreate'),
     url(r'^createplaces/$', csrf_exempt(views.PlacesCreate), name='createPlaces'),
 ]
